1fa57bb2774f7c9e1f01dc02f2bd35b3b8aca002
trendyjosh <trendyjosh@gmail.com>
2024-03-12 14:17:03 +0000

    Move enquiry email template into emails/ directory

--- app/Mail/EnquiryMail.php
+++ app/Mail/EnquiryMail.php
@@ -39,7 +39,7 @@ class EnquiryMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'mail.enquiry',
+            view: 'emails.enquiry',
         );
     }
 }
