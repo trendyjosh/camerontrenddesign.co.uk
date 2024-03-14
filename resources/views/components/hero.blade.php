cb89ae37b6e15962e773e49a01531924194b0e99
trendyjosh <trendyjosh@gmail.com>
2024-03-14 15:33:22 +0000

    Fix hero image not using new hero directory

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,5 +1,5 @@
 <div class="ct-main__hero-heading">
-    <img src="{{ asset($page->hero) }}" alt="{{ $page->title }} Image">
+    <img src="{{ asset('storage/' . $page->hero) }}" alt="{{ $page->title }} Image">
     <div class="ct-main__hero-overlay">
         <h1>{{ $page->title }}</h1>
         @if($page->sub_title)
