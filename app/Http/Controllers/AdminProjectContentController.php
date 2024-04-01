ed606f721d6d0187a14866c1ccf375bbd4383c85
trendyjosh <trendyjosh@gmail.com>
2024-04-01 18:51:51 +0100

    Create controller and request objects for project content

---
+++ app/Http/Controllers/AdminProjectContentController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProjectContentRequest;
+use App\Http\Requests\UpdateProjectContentRequest;
+use App\Models\ProjectContent;
+
+class AdminProjectContentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProjectContentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProjectContent $projectContent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProjectContent $projectContent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProjectContentRequest $request, ProjectContent $projectContent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProjectContent $projectContent)
+    {
+        //
+    }
+}
