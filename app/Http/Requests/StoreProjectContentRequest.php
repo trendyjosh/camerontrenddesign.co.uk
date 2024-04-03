b895d25f55609ceb16a3b42e74e71948411f54a2
trendyjosh <trendyjosh@gmail.com>
2024-04-03 18:03:21 +0100

    Set validation rules and messages for project content

--- app/Http/Requests/StoreProjectContentRequest.php
+++ app/Http/Requests/StoreProjectContentRequest.php
@@ -7,14 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class StoreProjectContentRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
@@ -22,7 +14,27 @@ class StoreProjectContentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'content' => ['array'],
+            'content.*.content' => ['required_without:content.*.file', 'string'],
+            'content.*.file' => ['required_without:content.*.content', 'file'],
+            'content.*.full' => ['boolean', 'required'],
+            'content.*.src' => ['string', 'nullable'],
+            'content.*.caption' => ['required_with:content.*.file', 'string'],
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'content.*.*.string' => 'This field must be a string',
+            'content.*.content.required_without' => 'The content field is required.',
+            'content.*.file.required_without' => 'The image field is required.',
+            'content.*.caption.required_with' => 'The image caption is required.',
         ];
     }
 }
