a18105147f49872e524b958677dfeccb92131538
trendyjosh <trendyjosh@gmail.com>
2024-04-03 21:40:46 +0100

    Fix validation logic
    Added nullable to required_with fields so that no value is needed when they are not required.
    
    Also updated the required_with on content and file to be required_with_all so that an existing image source can be supplied instead of a new image file each time.

--- app/Http/Requests/StoreProjectContentRequest.php
+++ app/Http/Requests/StoreProjectContentRequest.php
@@ -15,11 +15,14 @@ class StoreProjectContentRequest extends FormRequest
     {
         return [
             'content' => ['array'],
-            'content.*.content' => ['required_without:content.*.file', 'string'],
-            'content.*.file' => ['required_without:content.*.content', 'file'],
+            'content.*.id' => ['nullable', 'integer'],
+            'content.*.style' => ['nullable', 'string'],
+            'content.*.position' => ['nullable', 'integer'],
+            'content.*.content' => ['required_without_all:content.*.file,content.*.source', 'nullable', 'string'],
+            'content.*.file' => ['required_without_all:content.*.content,content.*.source', 'nullable', 'file'],
             'content.*.full' => ['boolean', 'required'],
-            'content.*.src' => ['string', 'nullable'],
-            'content.*.caption' => ['required_with:content.*.file', 'string'],
+            'content.*.source' => ['nullable', 'string'],
+            'content.*.caption' => ['required_with:content.*.file', 'nullable', 'string'],
         ];
     }
 
@@ -32,8 +35,8 @@ class StoreProjectContentRequest extends FormRequest
     {
         return [
             'content.*.*.string' => 'This field must be a string',
-            'content.*.content.required_without' => 'The content field is required.',
-            'content.*.file.required_without' => 'The image field is required.',
+            'content.*.content.required_without_all' => 'The content field is required.',
+            'content.*.file.required_without_all' => 'The image field is required.',
             'content.*.caption.required_with' => 'The image caption is required.',
         ];
     }
