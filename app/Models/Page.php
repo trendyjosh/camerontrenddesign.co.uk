ec95459e5092238a23693404bb1ee8c8674bd2d1
trendyjosh <trendyjosh@gmail.com>
2024-04-07 22:27:18 +0100

    Create method for hero upload
    Added a static method to handle hero image uploads which will resize and optimise the images before saving them to the page model.
    
    Replaced the file upload section in the page controller store method.

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -5,7 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Str;
+use Spatie\Image\Enums\Fit;
+use Spatie\Image\Image;
 
 class Page extends Model
 {
@@ -36,5 +39,23 @@ class Page extends Model
         return Attribute::make(
             get: fn (mixed $value) => strval(Str::of($this->title)->kebab()),
         );
+    }
+
+    /**
+     * Handle hero image upload. The width of the image will be reduced to 
+     * 1800px if too large and the resulting image will be optimised.
+     */
+    public static function uploadHero(UploadedFile $fileUpload): string | false
+    {
+        // Save image and get filepath
+        $uploadedImage = $fileUpload->store('hero', 'public');
+        if (!$uploadedImage) return false;
+        // Resize and optimise the image
+        $image = Image::load(storage_path('app/public') . "/" . $uploadedImage);
+        $image->fit(Fit::Max, 1800)
+            ->optimize()
+            ->save();
+        // Return filepath
+        return $uploadedImage;
     }
 }
