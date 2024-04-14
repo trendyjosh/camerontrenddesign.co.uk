229556b38a4a4b145195a30db6e4fa5437a72c4e
trendyjosh <trendyjosh@gmail.com>
2024-04-14 13:17:48 +0100

    Further validate project title
    Ensure there are no numbers in the title, the kebab-case route binding resolution does not properly format with numbers.
    
    There should be no need to include any numbers in a project title. This is why it makes most sense to remove the ability to include them rather than modify the route binding logic.

--- app/Http/Requests/UpdateProjectRequest.php
+++ app/Http/Requests/UpdateProjectRequest.php
@@ -14,11 +14,23 @@ class UpdateProjectRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => ['string'],
+            'title' => ['required', 'string', 'regex:/^[a-z ]+$/i'], // Only alphabetic and spaces
             'hero' => ['nullable', 'image'],
             'sub_title' => ['nullable', 'string'],
             'thumb' => ['nullable', 'image'],
             'status' => ['nullable', 'boolean'],
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'title.regex' => 'The title field may only contain alphabetic characters (a-z, A-Z) and spaces.',
         ];
     }
 }
