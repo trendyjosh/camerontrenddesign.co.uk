804814b82ce8fbce4929acb092eed84f7f548453
trendyjosh <trendyjosh@gmail.com>
2024-04-18 00:04:42 +0100

    Make settings available globally

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Settings\GeneralSettings;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Make settings available globally
+        View::share('site_phone', app(GeneralSettings::class)->site_phone);
+        View::share('site_email', app(GeneralSettings::class)->site_email);
     }
 }
