10723ad9d3e12c98dad3f3f661714f47a7b57e32
trendyjosh <trendyjosh@gmail.com>
2024-04-21 12:29:57 +0100

    Add controller for settings

---
+++ app/Http/Controllers/AdminGeneralSettingsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class AdminGeneralSettingsController extends Controller
+{
+    //
+}
