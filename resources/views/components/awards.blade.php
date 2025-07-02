6ab36d0236842c83fe4a29140c221b8166ecbfc8
trendyjosh <trendyjosh@gmail.com>
2025-07-02 15:05:43 +0100

    Add extra awards

--- resources/views/components/awards.blade.php
+++ resources/views/components/awards.blade.php
@@ -1,3 +1,5 @@
 <section class="section-bottom">
+    <a href="https://www.prolandscapermagazine.com/2024/09/26/pro-landscaper-30-under-30-the-next-generation-2024-winners-announced/" target="_blank"><img src="{{ asset('media/30_under_30.png') }}" alt="Pro Landscaper 30 under 30"></a>
+    <a href="https://corker.co.uk/showcase/" target="_blank"><img src="{{ asset('media/corker.png') }}" alt="Corker Show Garden Partner"></a>
     <a href="https://www.inchbald.co.uk" target="_blank"><img src="{{ asset('media/inchbald.jpg') }}" alt="Inchbald School of Design Logo"></a>
 </section>
