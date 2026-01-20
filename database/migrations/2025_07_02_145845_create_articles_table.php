cbcf6c042c9ccc47695f0af46c6487f76008efa5
trendyjosh <trendyjosh@gmail.com>
2026-01-20 20:57:18 +0000

    feat: add article page insert to migration

--- database/migrations/2025_07_02_145845_create_articles_table.php
+++ database/migrations/2025_07_02_145845_create_articles_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Page;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -20,6 +21,8 @@ return new class extends Migration
             $table->string('link')->nullable();
             $table->timestamps();
         });
+
+        $this->insertData();
     }
 
     /**
@@ -28,5 +31,19 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('articles');
+    }
+
+    /**
+     * Insert initial values needed for pages in
+     * navigation to work.
+     */
+    private function insertData(): void
+    {
+        // Press page
+        Page::create([
+            'title' => 'Press',
+            'sub_title' => null,
+            'hero' => ''
+        ]);
     }
 };
